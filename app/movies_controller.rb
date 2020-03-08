befd630bea7af3e55d9f20185d9e4c4972dfd4da
Chris Wade <swade781977@yahoo.com>
2020-03-08 22:10:07 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,12 +24,12 @@
     movie = Movie.create(attributes)
   end
 
-  def can_be_created_in_a_block(args = 1)
+  def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
     # If no arguments are passed, use default values:
     # title == "Home Alone"
     # release_date == 1990
   
-    Movie.create do |m|
+    Movie.create(args) do |m|
       
     end
   end
@@ -55,7 +55,9 @@
     # title == "Title"
     # release_date == 2000
     # director == "Me"
-    Movie.find_by(title: :title, release_date: :release_date, director: :director)
+    Movie.find_by(title: "Title") 
+    Movie.find_by(release_date: "2000") 
+    Movie.find_by(director: "Me")
   end
 
   def can_find_using_where_clause_and_be_sorted
@@ -67,16 +69,16 @@
   def can_be_found_updated_and_saved
     # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
     Movie.create(title: "Awesome Flick")
-    m = Movie.find_by(title: )
-    __
-    __
+    m = Movie.find_by(title: "Awesome Flick")
+    m.title = "Even Awesomer Flick"
+    m.save
   end
 
   def can_update_using_update_method
     # Update movie title to "Wat, huh?"
     Movie.create(title: "Wat?")
-    __
-    __
+    m = Movie.find_by(title: "Wat?")
+    m.update(title: "Wat, huh?")
   end
 
   def can_update_multiple_items_at_once
@@ -84,18 +86,18 @@
     5.times do |i|
       Movie.create(title: "Movie_#{i}", release_date: 2000+i)
     end
-    __
+    Movie.update_all(title: "A Movie")
   end
 
   def can_destroy_a_single_item
     Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-    __
-    __
+    m = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+    m.destroy
   end
 
   def can_destroy_all_items_at_once
     10.times do |i|
       Movie.create(title: "Movie_#{i}")
     end
-    __
+    Movie.destroy_all
   end
